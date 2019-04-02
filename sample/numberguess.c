4833cf51224096ffb27f48d7f88aceacd93b6b53
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-04-02 14:07:23 +0900

    remove u8 prefix

--- sample/numberguess.c
+++ sample/numberguess.c
@@ -7,7 +7,7 @@ int guess(const int lower, const int upper) {
     if(upper - lower <= 1) return lower;
     const int m = (upper + lower) / 2;
     char answer;                // ユーザーの回答
-    printf_s(u8"is the number greater than or equal to %d? y/n\n", m);
+    printf_s("is the number greater than or equal to %d? y/n\n", m);
     scanf_s("%c", &answer);
 
     // 改行文字の読み捨て
@@ -19,6 +19,6 @@ int guess(const int lower, const int upper) {
 }
 
 int main(void) {
-    puts(u8"choose a number between 0 and 99.");
-    printf_s(u8"the answer is %d!\n", guess(0, 100));
+    puts("choose a number between 0 and 99.");
+    printf_s("the answer is %d!\n", guess(0, 100));
 }
